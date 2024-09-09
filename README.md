57f676ee026eb1c5f4115d484d943d51ee0ff06e
unknown <karroch37>
2024-09-09 19:42:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Note
+For a quicksetup for setup raylib [this youtube video](https://www.youtube.com/watch?v=HPDLTQ4J_zQ) is a good start
---
+++ main.c
@@ -0,0 +1,25 @@
+#include "include/raylib.h"
+
+#define WIDTH 800
+#define HEIGHT 450
+#define BACKGROUND_COLOR \
+    CLITERAL(Color) { 38, 38, 38, 255 }
+
+int main(void) {
+    InitWindow(WIDTH, HEIGHT, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(BACKGROUND_COLOR);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
